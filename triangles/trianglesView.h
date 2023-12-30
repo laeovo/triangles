b2b8f129c297ccc93ba045bf69f8791ab8bb8e86
Leo Vornberger <leo.vornberger@gmx.de>
2023-12-31 00:17:56 +0100

    Initial Commit

---
+++ triangles/trianglesView.h
@@ -0,0 +1,12 @@
+//
+//  trianglesView.h
+//  triangles
+//
+//  Created by Leo Vornberger on 31.12.23.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface trianglesView : ScreenSaverView
+
+@end
